9d74804db3cc1f585cb76d2cabd51077a8b41628
OleksandrLypchei <oleksandrlypchei@gmail.com>
2020-12-24 23:50:59 +0200

    Файл с ответом на практическое задание

--- Module 12/Практикум 12 модуля.py
+++ Module 12/Практикум 12 модуля.py
@@ -1,4 +1,4 @@
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 money = float(input("Введите сумму вклада: "))
-deposit = [money*per_cent['ТКБ'], money*per_cent['СКБ'], money*per_cent['ВТБ'], money*per_cent['СБЕР']]
-print(deposit)
+deposit = [money*per_cent['ТКБ']/100, money*per_cent['СКБ']/100, money*per_cent['ВТБ']/100, money*per_cent['СБЕР']/100]
+print("Максимальная сумма, которую вы можете заработать —", max(deposit))
