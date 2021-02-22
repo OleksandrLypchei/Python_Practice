b65af1d74daa544b399b8de5e94138d1a09d0475
OleksandrLypchei <oleksandrlypchei@gmail.com>
2021-02-23 00:37:09 +0200

    Продолжение изучения работы с файлами

--- Module 15/Работа с файлами.py
+++ Module 15/Работа с файлами.py
@@ -22,7 +22,7 @@ def change_char(text, n=1):
     summary = ''
     for char in text:
         if char in alpha:
-            summary += alpha[(alpha.index(char) + n) % len(alpha) + 1]
+            summary += alpha[(alpha.index(char) + n) % len(alpha)]
         elif char in alpha_up:
             summary += alpha_up[(alpha_up.index(char) + n) % len(alpha)]
         else:
@@ -34,3 +34,55 @@ with open('name_file.txt', 'r') as inputFile:
     with open('output_file.txt', 'w') as outputFile:
         outputFile.write(change_char(inputFile.read()))
 
+
+import json
+
+# If we have dict, that we have to write in JSON file, we can use module "json".
+# Look at 'isAlive' and 'spouse' params.
+
+dictionary = {
+    'firstname': 'Коля',
+    'lastname': 'Петренко',
+    'isAlive': True,
+    'age': 19,
+    'address':
+    {
+        'region': 'Киевская обл.',
+        'city': 'Белая Церковь',
+        'street': 'ул. Набережна 42',
+        'postal': ''
+    },
+    'phoneNumbers':
+    [
+        {
+            'type': 'mob',
+            'number': '033355523'
+        },
+        {
+            'type': 'office',
+            'number': '031652654'
+        }
+    ],
+    'children': [],
+    'spouse' : None
+}
+
+jFile = open('jFile.json', 'w', encoding='utf8')              # json.dump(value, inFile, ensure_ascii=False, indent=4)
+json.dump(dictionary, jFile, ensure_ascii=False, indent=4)    # like .write but in translate for JSON-code
+jFile.close()
+
+jFile = open('jFile.json', encoding='utf8')            # json.load(value) like .read but with translation on JSON-code
+print('That"s what Json file look like in Python:')
+print(json.load(jFile))
+jFile.close()
+
+with open('jFile.json', 'a') as jFile:
+    stringInPython = r"True, False, None"
+    print(stringInPython, 'before .dumps method')
+    jFile.write(stringInPython)
+    stringInJson = json.dumps(stringInPython)
+    print(stringInJson, 'after .dumps method')
+    jFile.write(stringInJson)
+    stringInPythonAgain = json.loads(stringInJson)
+    print(stringInPythonAgain, 'after .loads method')
+    jFile.write(stringInPythonAgain)
